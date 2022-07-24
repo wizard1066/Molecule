d0391164af0163e11552ae1e34811dcac7608187
wizard1066 <mark.lucking@gmail.com>
2022-07-24 09:58:39 +0200

    Initial Commit

---
+++ Molecule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Molecule
+//
+//  Created by localuser on 24.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Molecule/MoleculeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoleculeApp.swift
+//  Molecule
+//
+//  Created by localuser on 24.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MoleculeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
